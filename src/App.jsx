637cadc46e15ac6e1e5392e4d8ec2737d024f89a
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2023-10-05 20:56:48 -0700

    Added like and dislike functions

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,25 @@ function App() {
   }, [likedSounds]);
 
   function handleLike(like, id) {
-    const liked = likedSounds
-    liked[id] = data[id]
-    if (useState.length < 10) {
-      updateLikedSounds({...liked})
+    var liked = likedSounds
+
+    if (like) {
+      liked[id] = data[id]
+      if (useState.length < 10) {
+        updateLikedSounds({...liked})
+      }
+    } else {
+      var hit = {}
+      Object.entries(liked).forEach((data) => {
+        if (data[0] != id+"") {
+          hit[data[0]] = data[1]
+        }
+      })
+      console.log("hit", hit)
+      updateLikedSounds({...hit})
     }
-    console.log(likedSounds);
   }
+
 
   return (
     <>
@@ -35,19 +47,28 @@ function App() {
       <h2>PEACH VORRIBURR, anytime, anywhere</h2>
       <div className="liked-section">
         {
-          Object.values(likedSounds).map((data, i) => {
-            return(<ButtonLiked key={i} sound={data.audio} name={data.name}/>)
+          (Object.values(likedSounds)).map((data, i) => {
+            return(<Button key={i} sound={data.audio} name={data.name}/>)
           })
         }
       </div>
       <div className="container">
         {
           data.map((data, i) => {
-            return(<Button key={i} kkey={i} sound={data.audio} name={data.name} handleLike={handleLike}/>)
+            const color = likedSounds.hasOwnProperty(data.id+"")?"#ff6b81":"#a4b0be"
+            return(<Button 
+                      key={i} 
+                      kkey={i} 
+                      sound={data.audio} 
+                      name={data.name} 
+                      handleLike={handleLike} 
+                      color={color}
+                    />)
           })
         }
       </div>
     </>
+    
   )
 }
 
--- src/Button.jsx
+++ src/Button.jsx
@@ -10,33 +10,17 @@ const Button = (props) => {
   }
 
   function handleClick() {
-    if (liked === false) {
-      props.handleLike(true, props.kkey)
-    } 
+    console.log(liked)
+    props.handleLike(!liked, props.kkey)
     updateLiked(!liked)
   }
 
   return (
     <div className="button" onClick={play}>
-      <FaHeart style={{color:liked?"#ff6b81":"#a4b0be"}} onClick={handleClick} className="heart"/>
+      <FaHeart style={{color:props.color}} onClick={handleClick} className="heart"/>
       {props.name}
     </div>
   );
 };
 
-
-function ButtonLiked(props){
-  function play() {
-    const audio = new Audio(props.sound);
-    audio.play();
-  }
-
-  return (
-    <div className="button" onClick={play}>
-      {props.name}
-    </div>
-  )
-}
-
 export default Button;
-export { ButtonLiked };
